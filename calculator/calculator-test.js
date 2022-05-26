644ab5a850a7824af6400c21f08f979b3b68ce85
Geena Jackson <geena.jackson96@gmail.com>
2022-05-26 14:22:42 -0500

    add calculateMontlyPayment() and test function

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({ amount: 1000, years: 2, rate: 0.03, })).toEqual("42.98");
 });
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
+// it("should return a result with 2 decimal places", function () {
+//   // ..
+// });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -21,17 +21,33 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let defaultValues = {
+    amount: document.getElementById("loan-amount"),
+    years: document.getElementById("loan-years"),
+    rate: document.getElementById("loan-rate"),
+  };
+  defaultValues.amount.innerText = "0";
+  defaultValues.years.innerText = "0";
+  defaultValues.rate.innerText = "0";
+  return calculateMonthlyPayment(defaultValues);
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let currentValues = getCurrentUIValues();
+  return calculateMonthlyPayment(currentValues);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let amount = values.amount;
+  let interestRate = values.rate / 12;
+  let payments = values.years * 12;
+  let monthlyPayment = ((amount * interestRate) / (1 - Math.pow((1 + interestRate), -payments)));
+  return monthlyPayment.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
